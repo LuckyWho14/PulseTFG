3c6082503bb8914ca14af5c7c2d25d030f30cf4f
Diego Juara <srjuara.es@gmail.com>
2025-05-26 21:59:22 +0200

    Clases para la BBDD

---
+++ Models/Ejercicio.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PulseTFG.Api.Models
+{
+    public class Ejercicio
+    {
+        [Key]
+        public int IdEjercicio { get; set; }
+
+        [Required]
+        public string Nombre { get; set; }
+
+        [Required]
+        public string Descripcion { get; set; }
+
+        [Required]
+        public string Video { get; set; }
+
+    }
+}
